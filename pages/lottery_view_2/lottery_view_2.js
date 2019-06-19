415f40784f0532e5b4386413b08cd546a5f51eef
likun_whu <2016302590230@whu.edu.cn>
2019-06-19 19:07:57 +0800

    我的

--- pages/lottery_view_2/lottery_view_2.js
+++ pages/lottery_view_2/lottery_view_2.js
@@ -1,4 +1,6 @@
 const app = getApp()
+var common = require("../../common/common.js")
+var util = require('../../utils/util.js')
 Page({
 
   /**
@@ -6,6 +8,10 @@ Page({
    */
   data: {
     top_height: 0,
+    data: [],
+    data_wait: [],
+    data_over: [],
+
   },
 
   /**
@@ -13,11 +19,44 @@ Page({
    */
   onLoad: function (options) {
     this.setData({
-      top_height: app.globalData.top_height
+      top_height: app.globalData.top_height,
+      data: common.create_lottery,
     })
+    console.log('我发起的:',this.data.data)
 
+    this.divide(this.data.data)
+    console.log(this.data)
   },
 
+  //0 未开奖，4 未中奖
+  divide: function (data) {
+    let data_wait_temp = []
+    let data_over_temp = []
+    let temp = data
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].status == 0) {
+
+        data[i].pic1 = "https://images2017.cnblogs.com/blog/1274477/201801/1274477-20180109191501941-2145316830.png"
+        data[i].pic2 = "https://images2017.cnblogs.com/blog/1274477/201801/1274477-20180109191501941-2145316830.png"
+        data[i].pic3 = "https://images2017.cnblogs.com/blog/1274477/201801/1274477-20180109191501941-2145316830.png"
+        data[i].time = util.tsFormatTime(data[i].time * 1000, 'Y-M-D h:m:s')
+        console.log(data[i].time)
+        data_wait_temp.push(data[i])
+      } else {
+        data[i].pic1 = "https://images2017.cnblogs.com/blog/1274477/201801/1274477-20180109191501941-2145316830.png"
+        data[i].pic2 = "https://images2017.cnblogs.com/blog/1274477/201801/1274477-20180109191501941-2145316830.png"
+        data[i].pic3 = "https://images2017.cnblogs.com/blog/1274477/201801/1274477-20180109191501941-2145316830.png"
+        data[i].time = util.tsFormatTime(data[i].time * 1000, 'Y-M-D h:m:s')
+        console.log(data[i].time)
+        data_over_temp.push(data[i])
+      }
+    }
+    this.setData({
+      data_wait: data_wait_temp,
+      data_over: data_over_temp,
+    })
+    console.log(this.data)
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
