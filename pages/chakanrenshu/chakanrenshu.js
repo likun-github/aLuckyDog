bb153c17244c6781a914a31f44fa18cc46be43fa
likun_whu <2016302590230@whu.edu.cn>
2019-06-25 17:37:57 +0800

    好的

--- pages/chakanrenshu/chakanrenshu.js
+++ pages/chakanrenshu/chakanrenshu.js
@@ -1,11 +1,12 @@
-// pages/chakanrenshu/chakanrenshu.js
-var app = getApp()
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    name: '',
+    pic: '',
     height_screen: 0,
     top_height: 0,
     background: 'rgba(255, 255, 255, 1)',
@@ -17,47 +18,117 @@ Page({
     fontSize: 16,
     iconHeight: 19,
     iconWidth: 58,
+    status: 0,
+    navHeight: 0,
+
+    data_user:[],
+
+
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-    var awardid=options.awardid;
-    var that = this;
-    
-    wx.request({
-      url: app.globalData.url + 'getAwardPeople',
-      data: {
-        'id': awardid
-      },
-      method: 'GET',
+    var that = this
+    wx.getSystemInfo({
       success: function (res) {
-
-        var cd = [];
-        for (var i = 0; i < res.data.data.length; i++) {
-          cd[i] = res.data.data[i].user__picture
-        }
-
+        console.log(res.windowHeight)
         that.setData({
-          canyu: cd,
-          cd1: cd.length
+          height_screen: res.windowHeight
         })
-        if (cd.length <= 7)
-          that.setData({
-            cd: cd.length
-          })
-        else that.setData({
-          cd: 7
+      }
+    })
+    this.setNavSize()
+
+    this.setData({
+      top_height: app.globalData.top_height
+    })
+    this.attached()
+    console.log(options.awardid)
+
+    wx.request({
+      url: app.globalData.url +'getAwardPeople',
+      data:{
+        id: options.awardid
+      },
+      success:function(res){
+        console.log(res)
+        that.setData({
+          data_user:res.data.data
         })
-        console.log(that.data.canyu)
-      },
-      fail: function (res) {
-        console.log('fail')
-      },
+
+        console.log(res.data.data)
+      }
     })
   },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+    this.animation = wx.createAnimation();
+    this.setData({
+      name: app.globalData.nickname,
+      pic: app.globalData.avatarUrl,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  // 返回事件
+  back: function () {
+    wx.navigateBack({
+      delta: 1
+    })
+    this.triggerEvent('back', {
+      back: 1
+    })
+
+  },
+
+
   setNavSize: function () {
     var that = this,
       sysinfo = wx.getSystemInfoSync(),
@@ -102,52 +173,6 @@ Page({
     that.setNavSize();
     that.setStyle();
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+ 
 })
