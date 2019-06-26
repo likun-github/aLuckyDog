e68adfdab7f24b7b48a23842b63a7062fd0769df
likun_whu <1761834475@qq.com>
2019-06-26 12:48:25 +0800

    h !

--- pages/lrjlottery/lrjlottery.js
+++ pages/lrjlottery/lrjlottery.js
@@ -31,6 +31,7 @@ Page({
       "/images/icn-zu@3x.png",
       "/images/icn-zu@3x.png",
     ],
+    level: '',//几等奖
     state: '', //个人对于奖项的状态
     awardid: '',
     imgurls: [],
@@ -41,9 +42,11 @@ Page({
     animation: '',
     date: '', //日期
     s: 1,
-    level: 0, //奖项状态
+    status: 0, //奖项状态
     kpnum: 0, //开奖人数||最多抽奖人数
-    userid: '' //用户登录
+    userid: '', //用户登录
+    status: 0,
+    navHeight: 0,
   },
   whole: function () {
     wx.navigateTo({
@@ -58,6 +61,7 @@ Page({
 * 生命周期函数--监听页面加载
 */
   onLoad: function (options) {
+    this.setNavSize()
     var that = this;
     wx.getSystemInfo({
       success: function (res) {
@@ -73,8 +77,9 @@ Page({
     this.attached()
     wx.hideShareMenu();
     //options.awardid
-    var awardid = options.awardid;
+    var awardid = 5;
     var userid = app.globalData.userid;
+
     that.setData({
       awardid: awardid
     })
@@ -86,7 +91,6 @@ Page({
       },
       method: 'GET',
       success: function (res) {
-
         var cd = [];
         for (var i = 0; i < res.data.data.length; i++) {
           cd[i] = res.data.data[i].user__picture
@@ -101,6 +105,7 @@ Page({
         console.log('fail')
       },
     })
+
     wx: wx.request({
       url: app.globalData.url + 'getUserAwardState',
       data: {
@@ -113,7 +118,8 @@ Page({
         var f3 = res.data.user_data;
         var f1 = res.data.award_data;
         var f2 = res.data.interpret;
-        console.log(f1, f2, f3)
+        var f4 = res.data.userWithaward;
+        console.log(f1, f2, f3, f4)
 
         var jpname = [f1[0].name1, f1[0].name2, f1[0].name3];
         var jpnum = [f1[0].num1, f1[0].num2, f1[0].num3];
@@ -131,7 +137,14 @@ Page({
           imgurls: images,
           status: f1[0].status, //抽奖状态
           name: f3.nickname,
-          pic: f3.picture
+          pic: f3.picture,
+        })
+        if (f4 != '')
+          that.setData({
+            level: f4[0].level
+          })
+        else that.setData({
+          level: 0
         })
         var s = f1[0].number;
         var image = that.data.imgurls;
@@ -159,9 +172,12 @@ Page({
       success: function (res) {
         console.log(res.data)
         state = res.data.state;
+        that.setData({
+          state: state,
+        })
         if (state == 0)
           that.setData({
-            could_join: false
+            could_join: false,
           })
       },
       fail: function (res) {
@@ -184,7 +200,8 @@ Page({
             var f3 = res.data.user_data;
             var f1 = res.data.award_data;
             var f2 = res.data.interpret;
-            console.log(f1, f2, f3)
+            var f4 = res.data.userWithaward;
+            console.log(f1, f2, f3, f4)
 
             var jpname = [f1[0].name1, f1[0].name2, f1[0].name3];
             var jpnum = [f1[0].num1, f1[0].num2, f1[0].num3];
@@ -202,7 +219,15 @@ Page({
               imgurls: images,
               status: f1[0].status, //抽奖状态
               name: f3.nickname,
-              pic: f3.picture
+              pic: f3.picture,
+
+            })
+            if (f4 != '')
+              that.setData({
+                level: f4[0].level
+              })
+            else that.setData({
+              level: 0
             })
             var s = f1[0].number;
             var image = that.data.imgurls;
@@ -230,9 +255,12 @@ Page({
           success: function (res) {
             console.log(res)
             state = res.data.state;
+            that.setData({
+              state: state,
+            })
             if (state == 0)
               that.setData({
-                could_join: false
+                could_join: false,
               })
           },
           fail: function (res) {
@@ -247,6 +275,25 @@ Page({
 
   },
 
+
+  // 通过获取系统信息计算导航栏高度
+  setNavSize: function () {
+    var that = this,
+      sysinfo = wx.getSystemInfoSync(),
+      statusHeight = sysinfo.statusBarHeight,
+      isiOS = sysinfo.system.indexOf('iOS') > -1,
+      navHeight;
+    if (!isiOS) {
+      navHeight = 48;
+    } else {
+      navHeight = 44;
+    }
+    that.setData({
+      status: statusHeight,
+      navHeight: navHeight
+    })
+
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -280,6 +327,59 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+
+    var awardid = this.data.awardid;
+    var userid = app.globalData.userid;
+    var that = this;
+    console.log(awardid, userid);
+    wx: wx.request({
+      url: app.globalData.url + 'getUserAwardState',
+      data: {
+        'awardid': awardid,
+        "userid": userid
+      },
+      method: 'GET',
+      success: function (res) {
+        var join = false;
+        var f3 = res.data.user_data;
+        var f1 = res.data.award_data;
+        var f2 = res.data.interpret;
+        var f4 = res.data.userWithaward;
+        console.log(f1, f2, f3, f4)
+
+        var jpname = [f1[0].name1, f1[0].name2, f1[0].name3];
+        var jpnum = [f1[0].num1, f1[0].num2, f1[0].num3];
+        var images = [f1[0].pic1, f1[0].pic2, f1[0].pic3];
+        var date = f1[0].time * 1000;
+        date = util.tsFormatTime(date, 'Y/M/D h:m:s');
+        that.setData({
+          index: f1[0].way, //开奖方式
+          jpname: jpname,
+          jpnum: jpnum,
+          date: date, //开奖时间
+          kpnum: f1[0].maxnum, //开奖人数
+          s: f1[0].number, //奖品个数
+          jpms: f1[0].information,
+          imgurls: images,
+          status: f1[0].status, //抽奖状态
+          name: f3.nickname,
+          pic: f3.picture,
+          level: f4[0].level
+        })
+        var s = f1[0].number;
+        var image = that.data.imgurls;
+        if (s == 1) image = [app.globalData.iurl + image[0]];
+        else if (s == 2) image = [app.globalData.iurl + image[0], app.globalData.iurl + image[1]];
+        else image = [app.globalData.iurl + image[0], app.globalData.iurl + image[1], app.globalData.iurl + image[2]];
+        that.setData({
+          imgurls: image
+        })
+      },
+      fail: function (res) {
+        console.log('fail')
+      },
+
+    })
 
   },
 
@@ -342,8 +442,11 @@ Page({
     that.setNavSize();
     that.setStyle();
   },
+
   join: function () {
     var that = this
+    var state = that.data.state;
+    console.log(state);
     wx.request({
       url: app.globalData.url + 'intoLottery',
       data: {
@@ -357,7 +460,6 @@ Page({
           title: res.data.interpret,
           content: '',
         })
-
       },
       fail: function (res) {
         console.log('fail')
@@ -367,64 +469,72 @@ Page({
 
   },
 
-  showmodels_tips: function () {
-    var that = this
-    //     wx.request({
-    //       url: app.globalData.url + 'intoShare',
-    //       data: {
-    //         'id': that.data.awardid
-    //       },
-    //       method: 'GET',
-    //       success: function (res) {
-    //         console.log(res.data.state)
-    //         that.setData({
-    //           state: res.data.state
-    //         })
-    //         if (that.data.state == 'success')
-    //           that.setData({
-    //             could_join: false
-    //           })
-    // }
-    //})
-    that.setData({
-      share_flag: true
+  handopen: function () {
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '你确定你要开奖了？',
+      confirmText: '确认开奖',
+      cancelText: '我再看看',
+      confirmColor: 'darkred',
+      cancelColor: 'darkgray',
+      success(res) {
+        if (res.confirm) {
+
+          wx.request({
+            url: app.globalData.url + 'openlottery',
+            data: {
+              'awardid': that.data.awardid
+            },
+            method: 'GET',
+            success: function (res) {
+              console.log(res)
+              wx.startPullDownRefresh();
+              setTimeout(function () {
+
+
+
+
+                wx.stopPullDownRefresh();
+
+
+              }, 800)
+            }
+          })
+
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
     })
-    this.translate()
   },
   go_to_lotteryCreate: function () {
     wx.navigateTo({
       url: '/pages/index/index',
     })
   },
-  share_lottery: function () {
-    this.setData({
-      share_flag: true,
-    })
-    this.translate()
-
-  },
-
-  translate: function () {
-    this.animation.translate(0, -120).step()
-    this.setData({
-      animation: this.animation.export()
-    })
-  },
-
-
-
-  cancel_share: function () {
-    this.setData({
-      share_flag: false,
-    })
-    this.translate_no()
-  },
-
-  translate_no: function () {
-    this.animation.translate(0, 120).step()
-    this.setData({
-      animation: this.animation.export()
+  share_lottery: function (res) {
+    if (this.data.awardid) {
+      if (res.from === 'button') {
+      }
+      return {
+        title: '转发',
+        path: '/pages/awardconfirm/awardconfirm?awardid=' + this.data.awardid,
+        success: function (res) {
+          console.log('成功', res)
+        }
+      }
+    } else wx.showModal({
+      title: '请稍后重试',
+      content: '网络出现异常请稍后重试',
     })
   },
+
+
+  go_to_advice: function () {
+    wx.navigateTo({
+      url: '/pages/advice/advice',
+    })
+  }
 
 })
